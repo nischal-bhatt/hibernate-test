f1c7c2dfb349be210e195eb4fb2ac4a7713694b4
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-03 13:04:47 +0800

    committing code for hibernate test

---
+++ src/org/koushik/javabrains/dto/HibernateTest.java
@@ -0,0 +1,30 @@
+package org.koushik.javabrains.dto;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+
+		UserDetailsNew ud = new UserDetailsNew();
+		
+		ud.setUserId(344262);
+		ud.setUserName("first nuser");
+		
+		
+		Configuration config = new Configuration().configure();
+		
+		SessionFactory sessionFactory = config.buildSessionFactory();
+	  Session s = sessionFactory.openSession();
+	  s.beginTransaction();
+	  
+	  s.save(ud);
+	  
+	  s.getTransaction().commit();
+	  
+	  
+	}
+
+}
---
+++ src/org/koushik/javabrains/dto/UserDetailsNew.java
@@ -0,0 +1,29 @@
+package org.koushik.javabrains.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class UserDetailsNew {
+
+	@Id
+	private int userId;
+	private String userName;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
